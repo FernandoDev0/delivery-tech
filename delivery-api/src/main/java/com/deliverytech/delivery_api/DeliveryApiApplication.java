2a83a7b7d1faccee388bd62ab99092facc7042ff
Fernando Aparecido <rodriguescalassio@gmail.com>
2025-08-20 23:26:46 -0300

    configuracoes do projeto Funcionando...

--- delivery-api/src/main/java/com/deliverytech/delivery_api/DeliveryApiApplication.java
+++ delivery-api/src/main/java/com/deliverytech/delivery_api/DeliveryApiApplication.java
@@ -3,7 +3,7 @@ package com.deliverytech.delivery_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.deliverytech.delivery_api")
 public class DeliveryApiApplication {
 
 	public static void main(String[] args) {
--- delivery-api/src/main/java/com/deliverytech/delivery_api/service/PedidoService.java
+++ delivery-api/src/main/java/com/deliverytech/delivery_api/service/PedidoService.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Optional;
 
+
 public interface PedidoService {
     
     // Operações básicas
