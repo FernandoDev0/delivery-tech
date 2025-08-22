d4ea36598de528560cb7a08c5f27bf560cedadc2
Fernando Aparecido <rodriguescalassio@gmail.com>
2025-08-22 20:50:40 -0300

    correcao da aula

--- delivery-api/src/main/java/com/deliverytech/delivery_api/dto/response/PedidoResponse.java
+++ delivery-api/src/main/java/com/deliverytech/delivery_api/dto/response/PedidoResponse.java
@@ -1,8 +1,8 @@
 package com.deliverytech.delivery_api.dto.response;
 
 
-//import com.deliverytech.delivery_api.model.StatusPedido;
-//import com.deliverytech.delivery_api.model.Endereco;
+import com.deliverytech.delivery_api.model.enums.StatusPedido;
+import com.deliverytech.delivery_api.model.Endereco;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -16,9 +16,9 @@ public class PedidoResponse {
     private Long id;
     private Long clienteId;
     private Long restauranteId;
-    //private Endereco enderecoEntrega;
+    private Endereco enderecoEntrega;
     private BigDecimal valorTotal;
-    //private StatusPedido status;
+    private StatusPedido status;
     private LocalDateTime dataPedido;
     private List<ItemPedidoResponse> itens;
 }
